7afafa5917d848a2994e2a1053089f198a7d5484
Gabriel Laurentino Tourinho <106614956+Laurentinoo@users.noreply.github.com>
2024-11-14 02:15:24 -0300

    Initial commit

---
+++ app/src/main/java/br/com/dlweb/maternidade/medico/MainFragment.java
@@ -0,0 +1,51 @@
+package br.com.dlweb.maternidade.medico;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import br.com.dlweb.maternidade.R;
+import br.com.dlweb.maternidade.mae.AdicionarFragment;
+import br.com.dlweb.maternidade.mae.ListarFragment;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.medico_fragment_main, container, false);
+        if (savedInstanceState == null) {
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameMedico, new br.com.dlweb.maternidade.medico.ListarFragment()).commit();
+        }
+
+        Button btnListar = v.findViewById(R.id.buttonListar);
+        btnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameMedico, new ListarFragment()).commit();
+            }
+        });
+
+        Button btnAdicionar = v.findViewById(R.id.buttonAdicionar);
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameMedico, new AdicionarFragment()).commit();
+            }
+        });
+
+        return v;
+    }
+}
